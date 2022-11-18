2d71992242e91458a4bf2abd87b4e9a1b94ed82a
Jaguar <snowjaguar625@gmail.com>
2022-11-18 16:39:21 +0900

    make it mote better

---
+++ build/precache-manifest.5a4a3c51afbe71df4bf9b3d23e648857.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "786e6ad3d2746df9201c",
+    "url": "/static/css/main.1cddd5b2.chunk.css"
+  },
+  {
+    "revision": "786e6ad3d2746df9201c",
+    "url": "/static/js/main.786e6ad3.chunk.js"
+  },
+  {
+    "revision": "1577e9f3a155441022eb",
+    "url": "/static/js/1.1577e9f3.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "67d272095806d75820fb416941b3ed1d",
+    "url": "/static/media/AileronRegular.67d27209.woff"
+  },
+  {
+    "revision": "c3c6301fb7ac3f02e39f88a1f15a22f5",
+    "url": "/static/media/AileronRegular.c3c6301f.woff2"
+  },
+  {
+    "revision": "b213ec5af81b23c7c6b57df58a7850ef",
+    "url": "/index.html"
+  }
+];
